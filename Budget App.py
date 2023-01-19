12ef8aebbe33f3b93249a0a3f724334591b1b4d5
Edgar Agustin Contreras <66283015+Edgaus@users.noreply.github.com>
2023-01-19 02:49:31 -0600

    Update Budget App.py
    Segundo boceto del programa a analizar, teminando una clase funcional

--- Budget App.py
+++ Budget App.py
@@ -1,40 +1,55 @@
 class Category:
 
+  budgets = {}
 
-    def __init__(self):
-        self.ledger = []
-        self.description = []
+  def __init__(self, category):
+
+    self.category = category
+    self.ledger = []
+    self.description = []
+
+    Category.budgets[self.category] = self.ledger
+
+  def deposit(self, deposit_amount, deposit_description=""):
+
+    self.ledger.append(deposit_amount)
+    self.description.append(deposit_description)
+
+    Category.budgets[self.category] = self.ledger
+
+    return dict(amount=self.ledger[-1], description=self.description[-1])
+
+  def withdraw(self, withdraw_amount, withdraw_description=''):
+    if withdraw_amount > sum(self.ledger):
+      return False
+    else:
+      self.ledger.append(-1 * withdraw_amount)
+      self.description.append(withdraw_description)
+
+    Category.budgets[self.category] = self.ledger
+
+    return True
     
-    def deposit(self, deposit_amount, deposit_description = "" ):
-
-        self.ledger.append(deposit_amount)
-        self.description.append(deposit_description)
-
-        self.budget = dict( amount = self.ledger[-1], description = self.description[-1]  )
-        return self.budget
+  def get_balance(self):
+    return sum(self.ledger)
     
-    def withdraw(self, withdraw_amount, withdraw_description = ''):
-        if withdraw_amount > sum(self.ledger):
-            return False
-        else:
-            self.ledger.append(-1*withdraw_amount)
-            self.description.append(withdraw_description)
+  def transfer(self, amount, budget_category):
 
-            self.budget = dict( amount = self.ledger[-1], description = self.description[-1]  )
+    self.budget_category = budget_category
 
+    if amount <= sum(self.ledger):
+      self.withdraw(amount, 'Transfer to {}'.format(budget_category.category))
+      budget_category.deposit(amount, 'Transfer from {}'.format(self.category))
+      return True
+    else:
+      return False
 
-            return True, self.budget
-    def get_balance(self):
-        return sum(self.ledger)
-
-
-food = Category()
-food.deposit(900, "deposit")
-food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
-
-print(food.get_balance())
+  def check_funds(self, amount):
+    if amount > sum(self.ledger):
+      return False
+    else:
+      return True
 
 
-
-
-
+def create_spend_chart(categories):
+  pass
