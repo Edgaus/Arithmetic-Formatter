78a715361d377ee15d9036b1f9754d6049e5725b
Edgar Agustin Contreras <66283015+Edgaus@users.noreply.github.com>
2023-01-19 05:05:48 -0600

    Update Budget App.py
    Final classes file

--- Budget App.py
+++ Budget App.py
@@ -1,43 +1,48 @@
+import re
 class Category:
-
-  budgets = {}
 
   def __init__(self, category):
 
     self.category = category
     self.ledger = []
-    self.description = []
-
-    Category.budgets[self.category] = self.ledger
 
   def deposit(self, deposit_amount, deposit_description=""):
 
-    self.ledger.append(deposit_amount)
-    self.description.append(deposit_description)
+    dict_movements = {
+      'amount': deposit_amount,
+      'description': deposit_description
+    }
+    self.ledger.append(dict_movements)
 
-    Category.budgets[self.category] = self.ledger
-
-    return dict(amount=self.ledger[-1], description=self.description[-1])
+    return self.ledger[-1]
 
   def withdraw(self, withdraw_amount, withdraw_description=''):
-    if withdraw_amount > sum(self.ledger):
+    self.suma = 0
+    for movement in self.ledger:
+      self.suma = movement['amount'] + self.suma
+
+    if withdraw_amount > self.suma:
       return False
     else:
-      self.ledger.append(-1 * withdraw_amount)
-      self.description.append(withdraw_description)
+      deposit_amount = -1 * withdraw_amount
+      dict_movements = {
+        'amount': deposit_amount,
+        'description': withdraw_description
+      }
+      self.ledger.append(dict_movements)
+      return True
 
-    Category.budgets[self.category] = self.ledger
-
-    return True
-    
   def get_balance(self):
-    return sum(self.ledger)
-    
+    self.suma = 0
+    for movement in self.ledger:
+      self.suma = movement['amount'] + self.suma
+    return self.suma
+
   def transfer(self, amount, budget_category):
 
     self.budget_category = budget_category
 
-    if amount <= sum(self.ledger):
+    if amount <= (self.get_balance()):
       self.withdraw(amount, 'Transfer to {}'.format(budget_category.category))
       budget_category.deposit(amount, 'Transfer from {}'.format(self.category))
       return True
@@ -45,11 +50,31 @@ class Category:
       return False
 
   def check_funds(self, amount):
-    if amount > sum(self.ledger):
+    if amount > self.get_balance():
       return False
     else:
       return True
 
+  def __str__(self):
+    espacio = int((30-len(self.category))/2)
+    lineas = espacio*"*" + self.category + espacio*"*"
+    for movement in self.ledger:
+        lineas += '\n'+ movement['description'][:23] + (23-len(movement['description'][:23]))*" "
+        
+        number_str = str(movement['amount'])
+        len_number = len(number_str)
+        if len_number > 7:
+            lineas += number_str[:7]
+        else:
+            decimal_number = re.findall('[0-9]+', number_str)
+            try:
+                if len(decimal_number[1])<3:
+                    lineas += (7-len(number_str))*" "+number_str
+                else:
+                    lineas += (3-len(decimal_number[0]))*" " + decimal_number[0] + "." + decimal_number[1]
 
-def create_spend_chart(categories):
-  pass
+            except:
+                lineas += (7-len(number_str))*" "+number_str
+
+    return lineas
+
