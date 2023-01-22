d82cc948b61ee604d6ade41a287c9b50668803f1
Edgar Agustin Contreras <66283015+Edgaus@users.noreply.github.com>
2023-01-21 23:39:44 -0600

    Update Budget App.py

--- Budget App.py
+++ Budget App.py
@@ -159,24 +159,3 @@ food.withdraw(15.55)
 entertainment.withdraw(33.40)
 business.withdraw(10.99)
 
-
-
-solution = (create_spend_chart([business, food ,entertainment]))
-
-expected =("Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
-)
-
-sol_cd = str.split( expected )
-sol_rpblem = str.split(   solution     )
-
-
-
-
-
-print(  sol_cd)
-print('\n \n \n \n')
-print(  sol_rpblem )
-
-print( sol_cd == sol_rpblem )
-
-print( solution == expected  )
